174b24b2e2b8bbb3a9f12e6c6b1e0bffbbbf2111
Jianhuan Zeng <37096176+JianhuanZeng@users.noreply.github.com>
2019-02-12 15:43:20 -0500

    Update E02_Summarize_DognitionData.sql

--- queries/E02_Summarize_DognitionData.sql
+++ queries/E02_Summarize_DognitionData.sql
@@ -1,18 +1,22 @@
 /* COUNT */
-SELECT COUNT(DISTINCT breed)
-FROM dogs;
-SELECT COUNT(*)
-  FROM complete_tests;
+SELECT COUNT(*) FROM dogs;
+SELECT COUNT(DISTINCT breed) FROM dogs;
+SELECT COUNT(*) FROM complete_tests;
 
 /* SUM */
-SELECT SUM(ISNULL(exclude))
-FROM dogs;
+SELECT SUM(ISNULL(exclude)) FROM dogs;
 
-/* AVG, MIN, MAX   */
+/* AVG, MIN, MAX  */
 SELECT test_name, 
-AVG(rating) AS AVG_Rating, 
-MIN(rating) AS MIN_Rating, 
-MAX(rating) AS MAX_Rating
-FROM reviews
-WHERE test_name IN ('Eye Contact Game', 'Slide')
-GROUP BY test_name;
+  AVG(rating) AS AVG_Rating, 
+  MIN(rating) AS MIN_Rating, 
+  MAX(rating) AS MAX_Rating
+  FROM reviews
+  WHERE test_name IN ('Eye Contact Game', 'Slide')
+  GROUP BY test_name;
+
+/* time */
+SELECT dog_guid, TIMESTAMPDIFF(minute,start_time,end_time) AS duration
+  FROM exam_answers LIMIT 1000;
+SELECT MIN(TIMESTAMPDIFF(minute,start_time,end_time)) AS MINduration, 
+  MAX(TIMESTAMPDIFF(minute, start_time, end_time)) AS MAXduration FROM exam_answers;
