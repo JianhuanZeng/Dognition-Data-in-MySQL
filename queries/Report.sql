1a373c214769fbb506ef5b81df9136153aaab3d9
Jianhuan Zeng <37096176+JianhuanZeng@users.noreply.github.com>
2019-02-12 15:43:15 -0500

    Update Report.sql

--- queries/Report.sql
+++ queries/Report.sql
@@ -7,8 +7,12 @@ SELECT COUNT(DISTINCT Dog_Guid) FROM dogs WHERE exclude=1;
 
 /* customer service */
 SELECT test_name, 
-AVG(rating) AS AVG_Rating, 
-MIN(rating) AS MIN_Rating, 
-MAX(rating) AS MAX_Rating
-FROM reviews
-GROUP BY test_name;
+  AVG(rating) AS AVG_Rating, 
+  MIN(rating) AS MIN_Rating, 
+  MAX(rating) AS MAX_Rating
+  FROM reviews
+  GROUP BY test_name;
+
+SELECT COUNT(DISTINCT test_name) FROM exam_answers;
+SELECT AVG(TIMESTAMPDIFF(minute,start_time,end_time)) AS AvgDuration 
+  FROM exam_answers GROUP BY test_name;
